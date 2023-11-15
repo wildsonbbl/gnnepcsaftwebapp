5f597cf105fc4dc54f40003e6e1ee5e6bd39fe71
wildsonbbl <wil_bbl@hotmail.com>
2023-11-15 23:58:26 +0000

    img not saved

--- gnnmodel/models.py
+++ gnnmodel/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-
-class PropImage(models.Model):
-    "Images for property plots."
-    den = models.FileField(default="images/den.png")
-    vp = models.FileField(default="images/vp.png")

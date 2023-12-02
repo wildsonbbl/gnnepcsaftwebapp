6aba8af6b1b02ef6dbd9fcf38ff95282dfc4a82d
wildsonbbl <wil_bbl@hotmail.com>
2023-12-02 06:00:47 +0000

    add db

--- gnnmodel/models.py
+++ gnnmodel/models.py
@@ -1,2 +1,11 @@
 "Module for django models (dbs)."
-# from django.db import models
+from django.db import models
+
+
+class GnnepcsaftDB(models.Model):
+    "Table at DB for gnnepcsaft predicted params."
+    inchi = models.CharField(max_length=255)
+    m = models.FloatField()
+    sigma = models.FloatField()
+    e = models.FloatField()
+    counting = models.IntegerField()

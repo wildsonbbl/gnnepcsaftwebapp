51e9f531035b252a54906c266d5cc1e2c7a69596
wildsonbbl <wil_bbl@hotmail.com>
2024-10-31 18:32:57 -0300

    del unsafe serve media code

--- webapp/urls.py
+++ webapp/urls.py
@@ -18,16 +18,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.static import serve
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("gnnmodel.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
-# urlpatterns += re_path(
-#     r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}
-# )
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

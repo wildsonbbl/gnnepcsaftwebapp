8f57840d39490570b7041f8e0416e7678d847765
wildsonbbl <wil_bbl@hotmail.com>
2024-10-31 14:03:24 -0300

    db_update at models

--- gnnmodel/models.py
+++ gnnmodel/models.py
@@ -11,3 +11,19 @@ class GnnepcsaftPara(models.Model):
     plot_den = models.CharField(max_length=50)
     plot_vp = models.CharField(max_length=50)
     plot_mol = models.CharField(max_length=50)
+
+
+def db_update(pred, inchi, comp, plots):
+    "Updates the gnnepcsaft db."
+    if len(comp) == 0:
+        new_comp = GnnepcsaftPara(
+            inchi=inchi,
+            m=pred[0],
+            sigma=pred[1],
+            e=pred[2],
+            plot_den=plots[0],
+            plot_vp=plots[1],
+            plot_mol=plots[2],
+        )
+        new_comp.save()
+        return [new_comp]

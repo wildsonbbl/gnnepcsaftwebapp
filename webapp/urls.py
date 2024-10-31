82c1d54eaf895ddf7c371eb95641e3b6c7684fe0
wildsonbbl <wil_bbl@hotmail.com>
2024-10-31 21:14:33 +0000

    serve from gcloud

--- webapp/urls.py
+++ webapp/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("gnnmodel.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
-    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
+# urlpatterns += re_path(
+#     r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}
+# )
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

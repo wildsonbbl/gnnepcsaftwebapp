acb6e4f098345ad7b21bb7fb2b34d9401faceed8
wildsonbbl <wil_bbl@hotmail.com>
2024-11-14 17:13:58 +0000

    plots

---
+++ gnnmodel/static/js/plot.js
@@ -0,0 +1,87 @@
+function getplot(dataname, xlegendpos, ytitle, id) {
+  var alldata = JSON.parse(dataname);
+  var trace1 = {
+    x: alldata["T"],
+    y: alldata["TML"],
+    mode: "markers",
+    type: "scatter",
+    name: "ThermoML Archive",
+    marker: {
+      symbol: "x",
+      color: "black",
+    },
+  };
+
+  var trace2 = {
+    x: alldata["T"],
+    y: alldata["GNN"],
+    mode: "lines",
+    type: "scatter",
+    name: "GNNePCSAFT",
+  };
+
+  var trace3 = {
+    x: alldata["T"],
+    y: alldata["RA"],
+    mode: "lines",
+    type: "scatter",
+    name: "Ramírez-Vélez et al. (2022)",
+  };
+  if (!alldata["RA"][0]) {
+    trace3 = {};
+  }
+
+  var layout = {
+    font: {
+      family: "Times New Roman",
+    },
+    legend: {
+      x: xlegendpos,
+      y: 1,
+      font: { family: "monospace", size: 10 },
+    },
+    paper_bgcolor: "#f8f9fa",
+    plot_bgcolor: "#f8f9fa",
+    margin: {
+      b: 50,
+      t: 50,
+      l: 50,
+      r: 20,
+    },
+    xaxis: {
+      title: {
+        text: "Temperature (K)",
+      },
+      linecolor: "black",
+      ticks: "inside",
+      minor: {
+        ticks: "inside",
+      },
+      mirror: true,
+      showline: true,
+      showgrid: false,
+    },
+    yaxis: {
+      title: {
+        text: ytitle,
+      },
+      linecolor: "black",
+      ticks: "inside",
+      minor: {
+        ticks: "inside",
+      },
+      mirror: true,
+      showline: true,
+      showgrid: false,
+    },
+    autosize: true,
+    showlegend: true,
+  };
+
+  var plot_data = [trace1, trace2, trace3];
+
+  Plotly.newPlot(id, plot_data, layout, {
+    responsive: true,
+    modeBarButtonsToRemove: ["select2d", "lasso2d"],
+  });
+}

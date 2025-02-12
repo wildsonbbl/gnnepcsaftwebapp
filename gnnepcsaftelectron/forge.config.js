d6138b1d76b25486b479acdeddd24d3f8b69f5c7
wildsonbbl <wil_bbl@hotmail.com>
2025-02-11 22:54:03 -0300

    electron app

---
+++ gnnepcsaftelectron/forge.config.js
@@ -0,0 +1,36 @@
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+const afterExtract = require("./afterExtract");
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    name: "GNNePCSAFT",
+    afterExtract: ["./afterExtract.js"],
+    icon: "./storelogo_scale_400_hf3_icon.ico",
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "win32"],
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};

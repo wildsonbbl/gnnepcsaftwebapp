6166fce175d733150e54e4a15c36e2a9435c68da
wildsonbbl <wil_bbl@hotmail.com>
2025-03-12 16:22:43 -0300

    add about

--- gnnmodel/urls.py
+++ gnnmodel/urls.py
@@ -1,10 +1,12 @@
 "Module for url config."
+
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", views.estimator, name="estimator"),
+    path("about/", views.about, name="about"),
     # path("", views.homepage, name="homepage"),
     # path("author/", views.authorpage, name="author"),
 ]

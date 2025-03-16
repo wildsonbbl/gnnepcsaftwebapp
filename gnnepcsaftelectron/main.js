4e5eee244714a7daf837d1e90e4330f2cac8ab24
wildsonbbl <wil_bbl@hotmail.com>
2025-03-16 13:43:32 -0300

    loading screen + more text.includes

--- gnnepcsaftelectron/main.js
+++ gnnepcsaftelectron/main.js
@@ -8,7 +8,6 @@ if (require("electron-squirrel-startup")) app.quit();
 
 const createWindow = async () => {
   const djangoBackend = startDjangoServer();
-  await waitForDjangoServer(djangoBackend);
 
   const win = new BrowserWindow({
     width: 768,
@@ -17,6 +16,10 @@ const createWindow = async () => {
   });
 
   win.menuBarVisible = false;
+
+  win.loadFile(path.join(__dirname, "index.html")); //from loading.io
+
+  await waitForDjangoServer(djangoBackend);
 
   win.loadURL("http://localhost:19770");
 
@@ -50,7 +53,7 @@ const startDjangoServer = () => {
   );
   const djangoBackend = spawn(
     appPath,
-    ["runserver", "--noreload", "localhost:19770"],
+    ["runserver", "--noreload", "--skip-checks", "localhost:19770"],
     { signal }
   );
 
@@ -77,11 +80,29 @@ const waitForDjangoServer = (djangoBackend) => {
   return new Promise((resolve, reject) => {
     const onData = (data) => {
       const text = data.toString();
-      console.log("Waiting for Django server...\n", text);
       if (text.includes("Starting development server")) {
         // Once we detect the server is running, remove this listener.
         djangoBackend.stdout.off("data", onData);
         resolve();
+      } else if (text.includes("Django")) {
+        // Once we detect the server is running, remove this listener.
+        djangoBackend.stdout.off("data", onData);
+        resolve();
+      } else if (text.includes("development server")) {
+        // Once we detect the server is running, remove this listener.
+        djangoBackend.stdout.off("data", onData);
+        resolve();
+      } else if (text.includes("webapp.settings")) {
+        // Once we detect the server is running, remove this listener.
+        djangoBackend.stdout.off("data", onData);
+        resolve();
+      } else if (text.includes("server at http://localhost:19770")) {
+        // Once we detect the server is running, remove this listener.
+        djangoBackend.stdout.off("data", onData);
+        resolve();
+      } else {
+        // Do nothing
+        console.log("Waiting for Django server to start...");
       }
     };
     djangoBackend.stdout.on("data", onData);

f42f9e2fa7303b9704483922fb5324e463380bc6
wildsonbbl <wil_bbl@hotmail.com>
2025-03-28 17:43:44 -0300

    bug fix

--- gnnepcsaftelectron/forge.config.js
+++ gnnepcsaftelectron/forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 module.exports = {
   packagerConfig: {
     asar: true,
-    name: "GNNePCSAFT",
+    name: "gnnepcsaft",
     icon: "./storelogo_scale_400_hf3_icon.ico",
     extraResource: ["../apppkg/dist/gnnepcsaftwebapp"],
   },

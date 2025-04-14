fc358c33d1fcd7f9afbb300a8de568ccfdf9c708
wildsonbbl <wil_bbl@hotmail.com>
2025-04-13 21:27:34 -0300

    consumer with live events

--- gnnmodel/consumers.py
+++ gnnmodel/consumers.py
@@ -1,34 +1,29 @@
 "consume module"
 
+import asyncio
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from google.genai.types import Content, Part
+from markdown import markdown
+
+from .chat_utils import start_agent_session
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     "Chat consumer"
 
+    live_events, live_request_queue = start_agent_session("session_01")
+
     async def receive(self, text_data=None, bytes_data=None):
         assert text_data is not None
         text_data_json = json.loads(text_data)
 
-        # The consumer ChatConsumer is synchronous while the channel layer
-        # methods are asynchronous. Therefore wrap the methods in async-to-sync
-        await self.channel_layer.send(  # type: ignore
-            self.channel_name,
-            {
-                "type": "chat.message",
-                "text": {"msg": text_data_json["text"], "source": "user"},
-            },
+        agent_to_client_task = asyncio.create_task(self.agent_to_client_messaging())
+        client_to_agent_task = asyncio.create_task(
+            self.client_to_agent_messaging(text_data_json["text"])
         )
-        # model response here
-        await self.channel_layer.send(  # type: ignore
-            self.channel_name,
-            {
-                "type": "chat.message",
-                "text": {"msg": "Bot says hello", "source": "assistant"},
-            },
-        )
+        await asyncio.gather(agent_to_client_task, client_to_agent_task)
 
     # Handles the chat.mesage event i.e. receives messages from the channel layer
     # and sends it back to the client.
@@ -36,3 +31,53 @@ class ChatConsumer(AsyncWebsocketConsumer):
         "send chat message"
         text = event["text"]
         await self.send(text_data=json.dumps({"text": text}))
+
+    async def agent_to_client_messaging(self):
+        """Agent to client communication"""
+
+        async for event in self.live_events:
+            # turn_complete
+            if event.turn_complete:
+                print("[TURN COMPLETE]")
+                break
+
+            if event.interrupted:
+                print("[INTERRUPTED]")
+                break
+
+            # Read the Content and its first Part
+            part = event.content and event.content.parts and event.content.parts[0]
+            if not part or not event.partial:
+                continue
+
+            # Get the text
+            text = event.content and event.content.parts and event.content.parts[0].text
+            if not text:
+                continue
+            assert isinstance(text, str)
+
+            # # Send the text to the client
+            await self.channel_layer.send(  # type: ignore
+                self.channel_name,
+                {
+                    "type": "chat.message",
+                    "text": {"msg": markdown(text), "source": "assistant"},
+                },
+            )
+            print(f"[AGENT TO CLIENT]: {text}")
+            await asyncio.sleep(2)
+
+    async def client_to_agent_messaging(self, text):
+        """Client to agent communication"""
+        await self.channel_layer.send(  # type: ignore
+            self.channel_name,
+            {
+                "type": "chat.message",
+                "text": {"msg": text, "source": "user"},
+            },
+        )
+
+        content = Content(role="user", parts=[Part.from_text(text=text)])
+        self.live_request_queue.send_content(content=content)
+        print(f"[CLIENT TO AGENT]: {text}")
+        await asyncio.sleep(2)

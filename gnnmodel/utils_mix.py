a86e3f8e1ba2e1c8927bbd8435588e8e68585be6
wildsonbbl <wil_bbl@hotmail.com>
2026-07-11 20:53:32 -0300

    raise error for diagram plots

--- gnnmodel/utils_mix.py
+++ gnnmodel/utils_mix.py
@@ -529,6 +529,7 @@ def mix_vle(
         )
     except RuntimeError as exc:
         logger.debug("mix_vle: Runtime Error at pressure=%.4f: %s", pressure, exc)
+        raise
     except BaseException as exc:  # pylint: disable=W0718
         exception_type = type(exc).__name__
         if exception_type == "PanicException":
@@ -602,6 +603,7 @@ def mix_lle(
             params.mole_fractions,
             exc,
         )
+        raise
     except BaseException as exc:  # pylint: disable=W0718
         exception_type = type(exc).__name__
         if exception_type == "PanicException":
